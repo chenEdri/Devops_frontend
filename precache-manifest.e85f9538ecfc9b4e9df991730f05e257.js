1ce01efeb177df6ada3fa2cba6707e3661049f44
chen edri <chenedri22@gmail.com>
2021-05-09 20:32:51 +0300

    Updates

---
+++ precache-manifest.e85f9538ecfc9b4e9df991730f05e257.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc0ea41188f161f05089e573756d891",
+    "url": "/Devops_frontend/index.html"
+  },
+  {
+    "revision": "db4b32495f7ee23e690e",
+    "url": "/Devops_frontend/static/css/main.797cfa4e.chunk.css"
+  },
+  {
+    "revision": "dae20020b6d6d022b156",
+    "url": "/Devops_frontend/static/js/2.3c662a31.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Devops_frontend/static/js/2.3c662a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4b32495f7ee23e690e",
+    "url": "/Devops_frontend/static/js/main.72de20e1.chunk.js"
+  },
+  {
+    "revision": "17921a4c829e3d9fd899",
+    "url": "/Devops_frontend/static/js/runtime-main.45ee8866.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/Devops_frontend/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/Devops_frontend/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/Devops_frontend/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "9a77fbaa85fa42b73e3b96399daf49c5",
+    "url": "/Devops_frontend/static/media/Lato-Thin.9a77fbaa.ttf"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/Devops_frontend/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/Devops_frontend/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Devops_frontend/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
